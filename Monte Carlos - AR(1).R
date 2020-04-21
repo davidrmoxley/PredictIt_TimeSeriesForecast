c4c8d02ea4d36b790a843c9f5a5f57c157d48fc4
David Rex Moxley <drmoxley2@gmail.com>
2020-04-21 13:21:04 -0400

    Add files via upload

---
+++ Monte Carlos - AR(1).R
@@ -0,0 +1,3 @@
+# Monte Carlo AR(1)
+# 95% probability interval for x_t if x_t = phi*e_t-1 and e_t ~ IID(0,1)
+
---
+++ Monte Carlos - Autocorrelation.R
@@ -0,0 +1,2 @@
+# Monte Carlos - Autocorrelation Function
+# Check NULL hypothesis of variance
